9b5a967835e9117f09a350d30498f386c7d46c56
thanayyy <thanayyy5998@gmail.com>
2024-02-21 21:17:27 +0700

    testnode

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path")
 
 
 //const base_url = "http://node57329-nodepem.proen.app.ruk-com.cloud"
-const base_url = "http://localhost:3000"
+const base_url = "http://node58797-thanayutnode.proen.app.ruk-com.cloud:11561"
 //const base_url = "http://localhost:3000"
 app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
