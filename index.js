fc8e0372784e3c17e13b30f73233c642987f7e22
thanayyy <thanayyy5998@gmail.com>
2024-02-22 16:10:42 +0700

    .

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const path = require("path")
 
 
 
-//const base_url = "http://node57329-nodepem.proen.app.ruk-com.cloud"
-const base_url = "http://node58797-thanayutnode.proen.app.ruk-com.cloud"
-//const base_url = "http://localhost:3000"
+//const base_url = "http://node58797-thanayutnode.proen.app.ruk-com.cloud"
+const base_url = "http://localhost:3000"
+
 app.set("views", path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
